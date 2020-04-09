85d2faaf353b2c6e7ee441ff64e8622d2f302e93
HassenH1 <hasansaid51@gmail.com>
2020-04-09 14:42:37 -0700

    lifiting property finally achieved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,14 @@ import { Component, Input } from '@angular/core';
 export class AppComponent {
   title = 'signupLogin';
   email = ""
+  
+
+  onActivate(componentReference) {
+    console.log(componentReference, ",------------------whats this refering too")
+    //Below will subscribe to the searchItem emitter
+    componentReference.childEvent.subscribe((data) => {
+      console.log(data)
+      this.email = data
+    })
+ }
 }
